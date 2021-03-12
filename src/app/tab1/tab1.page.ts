db74e6babbc50249a3d192a99de7f1968596d425
JayPatel <jaybpatidar@gmail.com>
2021-03-12 19:39:45 +0530

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
+import homeData from '../../assets/mockdata/home.json';
+import { DrawerService } from '../services/drawer.service';
+import { ModalPage } from '../modal/modal.page';
+import { modalEnterAnimation, modalLeaveAnimation } from '../modal-animation';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +11,34 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  sections = homeData.sections;
+  spotlight = homeData.spotlight;
+
+  opts = {
+    slidesPerView: 2.4,
+    spaceBetween: 10,
+    freeMode: true
+  };
+
+  constructor(
+    private modalCtrl: ModalController,
+    private drawerService: DrawerService
+    ) {
+  }
+
+  openInfo(series) {
+    this.drawerService.openDrawer(series.title);
+  }
+
+  async openCategories() {
+    const modal = await this.modalCtrl.create({
+      component: ModalPage,
+      cssClass: 'transparent-modal',
+      enterAnimation: modalEnterAnimation,
+      leaveAnimation: modalLeaveAnimation
+    });
+
+    await modal.present();
+  }
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
+import { DrawerComponent } from '../components/drawer/drawer.component';
+import { DrawerService } from '../services/drawer.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  @ViewChild(IonTabs) tabs;
+  selected = '';
 
-  constructor() {}
+  @ViewChild(DrawerComponent) drawer: DrawerComponent;
+  backdropVisible = false;
 
+  constructor(private drawerService: DrawerService, private changeDetectorRef: ChangeDetectorRef) {
+    this.drawerService.drawerOpen.subscribe(drawerData => {
+      if (drawerData && drawerData.open) {
+        this.drawer.openDrawer(drawerData.title);
+      }
+    });
+  }
+
+  setSelectedTab() {
+    this.selected = this.tabs.getSelected();
+  }
+
+  closeDrawer() {
+    this.drawer.closeDrawer();
+  }
+
+  toggleBackdrop(isVisible) {
+    this.backdropVisible = isVisible;
+    this.changeDetectorRef.detectChanges();
+  }
 }
